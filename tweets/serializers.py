39a5397600c517d25a9352e239163f1ff3579852
omokehinde <engr.omokehinde@gmail.com>
2021-07-20 17:19:48 +0100

    Tweet Actions
    Tweet action serializer. Tweet action view to manage tweet actions

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -4,6 +4,17 @@ from rest_framework import serializers
 from .models import Tweet
 
 MAX_TWEET_LENGHT = settings.MAX_TWEET_LENGHT
+TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
+
+class TweetActionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    action = serializers.CharField()
+
+    def validate_action(self, value):
+        value = value.lower().strip()
+        if not value in TWEET_ACTION_OPTIONS:
+            raise serializers.ValidationError('This is not a valid action for tweet')
+        return value
 
 class TweetSerializer(serializers.ModelSerializer):
     class Meta:
